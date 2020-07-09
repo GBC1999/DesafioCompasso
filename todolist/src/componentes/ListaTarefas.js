be1f94d74b7620b17702a48f1e3ddd6ab8971012
Gabriel Bauer Ciancaglio <bielbauer.gbc@gmail.com>
2020-07-09 07:47:42 -0300

    Reajuste no main.js e transferencia do conteudo do cadastroTarefa para o main.js

--- todolist/src/componentes/ListaTarefas.js
+++ todolist/src/componentes/ListaTarefas.js
@@ -6,13 +6,15 @@ import Tarefa from "./Tarefa";
 export default function ListaTarefas({ tarefas, alternaFeito, removeTarefa }) {
     return (
         <List>
-            {tarefas.map(lista => (
-                <Tarefa
-                    key={lista.id}
-                    lista={lista}
-                    alternaFeito={alternaFeito}
-                    removeTarefa={removeTarefa} />
-            ))}
+            {tarefas.map(lista => {
+                return (
+                    <Tarefa
+                        key={lista.id}
+                        lista={lista}
+                        alternaFeito={alternaFeito}
+                        removeTarefa={removeTarefa} />
+                );
+            })}
         </List>
     )
 }
--- todolist/src/main.js
+++ todolist/src/main.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react' //useEffect permite fornecer funcionalidades aos dados
 import './main.css';
-import cadastroTarefa from './componentes/cadastroTarefa';
+// import cadastroTarefa from './componentes/cadastroTarefa';
 import ListaTarefas from "./componentes/ListaTarefas";
 import Typography from "@material-ui/core/Typography";
+//import {v4 as uuid} from "uuid";
+import { Button, TextField } from "@material-ui/core";
 
-const LOCAL_STORAGE_KEY = "react-TodoList-tarefas";
+const LOCAL_STORAGE_KEY = "react-todolist-tarefas";
 
 export default function Main() {
     const [tarefas, setTarefas] = useState([]); //array de dois itens, sendo tarefa o estado, setTarefas passador de parametro(s) e useState funcao de atualizar parametro(s) 
@@ -20,9 +22,9 @@ export default function Main() {
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tarefas));
     }, [tarefas]);
 
-    function adicionaTarefa(lista) { //adiciona uma tarefa
-        setTarefas([lista, ...tarefas]);
-    }
+    // function adicionaTarefa(lista) { //adiciona uma tarefa
+    //     setTarefas([lista, ...tarefas]);
+    // }
 
     function alternaFeito(id) { //alterna a condicao do feito
         setTarefas(
@@ -42,16 +44,45 @@ export default function Main() {
         setTarefas(tarefas.filter(lista => lista.id !== id));
     }
 
+    function handleSubmit(e) { //entrada da tarefa
+         e.preventDefault();
+        // if (lista.tarefa.trim()) { //trim remove os espacos em branco
+        //     adicionaTarefa({ ...lista, id: uuid.v4() });//uuid pacote de id unico e v4 faz ele gerar o id
+        //     //reinicia a entrada de tarefa 
+        //     setLista({ ...lista, tarefa: "" });
+        // }
+    }
+
+    const [lista, setLista] = useState ([{
+        id: '', //identificador de tarefa
+        tarefa: "",  //parametro
+        feito: false   //validador
+    }]);
+
+    function handleInputChange(e) { //atualiza tarefa
+        setLista({ ...lista, tarefa: e.target.value });
+        console.log(lista);
+    }
+
     return ( //campo de inserir tarefa
         <div className="Main">
             <Typography style={{ padding: 10 }} variant="h3">
                 Lista de Tarefas
-                </Typography>
-            <cadastroTarefa adicionaTarefa={adicionaTarefa} />
+           </Typography>
+           <form className="cadastro-tarefa" onSubmit={handleSubmit}>
+            <TextField
+                label="Tarefa"
+                name="tarefa"
+                type="text"
+                value={lista.tarefa}
+                onChange={handleInputChange}
+            />
+            <Button type="submit">Adicionar</Button>
+          </form>
             <ListaTarefas
-                tarefas={tarefas}
+                tarefas={lista}
                 alternaFeito={alternaFeito}
-                removeTarefa={removeTarefa} 
+                removeTarefa={removeTarefa}
             />
         </div>
     );
